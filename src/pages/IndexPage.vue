85a1c7e2f3a3de89d56242ff2d61dc61572c6530
kyle <wilsonatuel422@gmail.com>
2024-09-24 17:16:12 +0800

    added login features

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -3,22 +3,25 @@
     <q-page class="flex justify-center items-start">
       <div class="column q-mb-md">
         <div class="col-12 col-md-4 col-lg-4 q-pa-md">
-          <div class="text-h4 q-pa-md ">Login muna Bossing</div>
+          <div class="text-h4 q-pa-md">Login muna Bossing</div>
           <div>Please Login to Continue</div>
           <div class="column">
-            <q-input required="true" outlined label="Enter username *" v-model="form.username.value" :error="form.username.error"
-              :error-message="form.username.msg">
+            <q-input required="true" outlined label="Enter username *" v-model="form.username.value"
+              :error="form.username.error" :error-message="form.username.msg">
               <template v-slot:prepend>
                 <q-icon name="email"></q-icon>
               </template>
             </q-input>
-            <q-input type="password" class="q-mt-sm" outlined label="Enter password *" v-model="form.password.value"
-              :error="form.password.error" :error-message="form.password.msg">
+
+            <q-input required="true" :type="passwordFieldType" class="q-mt-sm" outlined label="Enter password *"
+              v-model="form.password.value" :error="form.password.error" :error-message="form.password.msg">
               <template v-slot:prepend>
-                <q-icon name="lock "></q-icon>
+                <q-icon name="lock"></q-icon>
               </template>
               <template v-slot:append>
-                <q-icon name="visibility_off "></q-icon>
+                <q-icon :name="isPasswordVisible ? 'visibility' : 'visibility_off'" 
+                        @click="togglePasswordVisibility"
+                        :color="isPasswordVisible ? 'primary' : 'grey'" />
               </template>
             </q-input>
           </div>
@@ -30,44 +33,53 @@
               Login
             </q-btn>
           </div>
-          <div clas>
-          </div>
         </div>
-        
       </div>
     </q-page>
   </div>
-
 </template>
 
-<script>
 
+<script>
 export default {
   data() {
     return {
+      isPasswordVisible: false,
       form: {
         username: {
           value: '',
-          required: true,
+          error: false,
+          msg: ''
         },
         password: {
           value: '',
-          required: true
+          error: false,
+          msg: ''
         }
       }
-    };
+    }
+  },
+  computed: {
+    passwordFieldType() {
+      return this.isPasswordVisible ? 'text' : 'password';
+    }
   },
   methods: {
-    goToSignup() {
-      this.$router.push('/signup');  // Programmatic navigation to the signup page
+    togglePasswordVisibility() {
+      this.isPasswordVisible = !this.isPasswordVisible;
     },
-    onsubmit() {
+    goToSignup() {
+      this.$router.push('/signup');
+    },
+    submitLogin() {
       console.log('Form data:', this.form);
+      // Add your login logic here
     }
   }
-
 }
 </script>
+
+
 <style>
 .page-container {
   background-image: url('src/assets/bg.jpg');
